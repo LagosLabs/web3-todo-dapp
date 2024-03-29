a7c4b8ce7944e52c249e70f42cd1e411bb532f54
lagoslabs <gregory@lagoslabs.dev>
2024-03-29 13:54:00 -0500

    First commit

---
+++ src/context/Web3Provider.tsx
@@ -0,0 +1,28 @@
+"use client"
+
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultConfig, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { polygonMumbai } from "wagmi/chains";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactNode } from "react";
+
+const config = getDefaultConfig({
+  appName: "cashapp",
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+  chains: [polygonMumbai],
+  ssr: true,
+});
+
+const queryClient = new QueryClient();
+const Web3Provider = ({ children }: { children: ReactNode }) => {
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider>{children}</RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
+
+export default Web3Provider;
